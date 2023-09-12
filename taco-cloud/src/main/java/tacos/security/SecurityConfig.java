082a954fb7af2138bdaedfe3d61dc42f003320d3
98kunwu <98kunwu@gmail.com>
2023-09-12 11:15:01 +0900

    [chapter4] JPA기반 사용자인증

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -2,46 +2,56 @@ package tacos.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation
+		.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation
+		.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web
+		.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web
+		.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-import javax.sql.DataSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+
 
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/design", "/orders")
-                .access("hasRole('ROLE_USER')")
-                .antMatchers("/", "/**").access("permitAll")
-                .and()
-                .httpBasic();
-    }
-
-    @Autowired
-    DataSource dataSource;
-    @Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
-		auth.ldapAuthentication()
-				.userSearchBase("ou=people")
-				.userSearchFilter("(uid={0})")
-				.groupSearchBase("ou=groups")
-				.groupSearchFilter("(member={0})")
-				.contextSource()
-				.root("dc=tacocloud,dc=com")
-				.ldif("classpath:users.ldif")
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+				.antMatchers("/design", "/orders")
+				.access("hasRole('ROLE_USER')")
+				.antMatchers("/", "/**").access("permitAll")
 				.and()
-				.passwordCompare()
-				.passwordEncoder(new BCryptPasswordEncoder())
-				.passwordAttribute("userPasscode");
+				.formLogin()
+				.loginPage("/login")
+				.and()
+				.logout()
+				.logoutSuccessUrl("/")
+				.and()
+				.csrf();
+	}
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth
+				.userDetailsService(userDetailsService)
+				.passwordEncoder(encoder());
+
 	}
 }
