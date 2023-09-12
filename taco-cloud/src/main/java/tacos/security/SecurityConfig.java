858abb65a4361022645b33648a2844b25bbac605
98kunwu <98kunwu@gmail.com>
2023-09-12 10:30:50 +0900

    [chapter4] LDAP

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 
 
@@ -21,16 +25,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.inMemoryAuthentication()
-                .withUser("user1")
-                .password("{noop}password1")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("user2")
-                .password("{noop}password2")
-                .authorities("ROLE_USER");
-    }
+    @Autowired
+    DataSource dataSource;
+    @Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.ldapAuthentication()
+				.userSearchBase("ou=people")
+				.userSearchFilter("(uid={0})")
+				.groupSearchBase("ou=groups")
+				.groupSearchFilter("(member={0})")
+				.contextSource()
+				.root("dc=tacocloud,dc=com")
+				.ldif("classpath:users.ldif")
+				.and()
+				.passwordCompare()
+				.passwordEncoder(new BCryptPasswordEncoder())
+				.passwordAttribute("userPasscode");
+	}
 }
