889ef5c1d4223cfcaf091d32546a4fa7956ea59b
98kunwu <98kunwu@gmail.com>
2023-09-12 22:04:50 +0900

    [chapter5] END

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
@@ -22,16 +24,18 @@ import org.springframework.validation.Errors;
 @RequestMapping("/orders")
 @SessionAttributes("order")
 public class OrderController {
-	
+	private OrderProps props;
+
 	private OrderRepository orderRepo;
-	
-	public OrderController(OrderRepository orderRepo) {
+
+	public OrderController(OrderRepository orderRepo, OrderProps props) {
 		this.orderRepo = orderRepo;
+		this.props = props;
 	}
-	
+
 	@GetMapping("/current")
 	public String orderForm(@AuthenticationPrincipal User user,
-			@ModelAttribute Order order) {
+							@ModelAttribute Order order) {
 		if (order.getDeliveryName() == null) {
 			order.setDeliveryName(user.getFullname());
 		}
@@ -49,19 +53,29 @@ public class OrderController {
 		}
 		return "orderForm";
 	}
-	
+
 	@PostMapping
 	public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus
-							, @AuthenticationPrincipal User user) {
+			, @AuthenticationPrincipal User user) {
 		if (errors.hasErrors()) {
 			return "orderForm";
 		}
-		
+
 		order.setUser(user);
-		
+
 		orderRepo.save(order);
 		sessionStatus.setComplete();
-		
+
 		return "redirect:/";
+	}
+
+	@GetMapping
+	public String ordersForUser(
+			@AuthenticationPrincipal User user, Model model) {
+
+		Pageable pageable = PageRequest.of(0, props.getPageSize());
+		model.addAttribute("orders",
+				orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+		return "orderList";
 	}
 }
